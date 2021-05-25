2eafeda054e11cae07ba5917cbeaf543914bde76
Robert <robertdoneux@gmail.com>
2021-05-25 22:07:52 +0100

    [main] bulks out test database

--- bin/main/db/migration/V5_1__add_test_slider_collection_relations.sql
+++ bin/main/db/migration/V5_1__add_test_slider_collection_relations.sql
@@ -1,4 +1,18 @@
 INSERT INTO collection_sliders (slider_id, collection_id)
 VALUES
 ('5913905d-059a-4b07-93e2-c97a1428c78e', 'e3228e77-b31b-40e6-9bfe-0e4359fb1455'),
-('5913905d-059a-4b07-93e2-c97a1428c78e', 'f8e6f439-e655-496c-8a50-b868ac933ecf');
+('5913905d-059a-4b07-93e2-c97a1428c78e', 'f8e6f439-e655-496c-8a50-b868ac933ecf'),
+
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', 'e3228e77-b31b-40e6-9bfe-0e4359fb1455'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', 'f8e6f439-e655-496c-8a50-b868ac933ecf'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', '58c59c51-5b83-4342-af7e-fa33579033f8'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', 'c3441915-3654-46cc-8fb9-bcb50fbbe731'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', '9f002d09-20f7-4094-bbae-2ed1d0e19c2e'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', 'c91a680d-54dc-444e-9e38-079a66c21f2f'),
+
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', 'c91a680d-54dc-444e-9e38-079a66c21f2f'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', '9f002d09-20f7-4094-bbae-2ed1d0e19c2e'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', 'c3441915-3654-46cc-8fb9-bcb50fbbe731'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', '58c59c51-5b83-4342-af7e-fa33579033f8'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', 'e3228e77-b31b-40e6-9bfe-0e4359fb1455'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', 'f8e6f439-e655-496c-8a50-b868ac933ecf');
--- src/main/resources/db/migration/V3_1__add_test_slider_data.sql
+++ src/main/resources/db/migration/V3_1__add_test_slider_data.sql
@@ -3,4 +3,6 @@
 -----------------
 insert into slider (id, title, colour)
 VALUES 
-('5913905d-059a-4b07-93e2-c97a1428c78e', 'nature & life', '#47742c');
+('5913905d-059a-4b07-93e2-c97a1428c78e', 'Nature & Life', '#47742c'),
+('5a75137b-475d-4d98-aaee-40d0e78f5bc1', 'Urban & City Life', '#5d5d5d'),
+('a6fb04af-f866-4e49-9d0a-db4bba8e8c8d', 'Odds & Sods', '#dae0e6');
