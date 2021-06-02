6d47cf8a2da658ba79bc39f46a15a78505bb0c31
Robert <robertdoneux@gmail.com>
2021-06-02 19:10:08 +0100

    [main] removes lombok from supporting classes

--- src/main/java/com/roboautomator/app/component/util/ValidationError.java
+++ src/main/java/com/roboautomator/app/component/util/ValidationError.java
@@ -1,13 +1,28 @@
 package com.roboautomator.app.component.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@ToString
-@AllArgsConstructor
 public class ValidationError {
     private final String field;
     private final String error;
+
+    public ValidationError(String field, String error) {
+        this.field = field;
+        this.error = error;
+    }
+
+    public String toString() {
+        try {
+            return StringHelper.classToString(this);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getError() {
+        return error;
+    }
 }
--- src/test/java/com/roboautomator/app/component/util/StringHelperTest.java
+++ src/test/java/com/roboautomator/app/component/util/StringHelperTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import static com.roboautomator.app.component.util.StringHelper.cleanString;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import com.roboautomator.app.component.image.ImageEntity;
+
 class StringHelperTest {
 
     @Test
@@ -33,5 +38,38 @@ class StringHelperTest {
         var testString = "He\nll\ro\t\nWor\r\rl\t\td";
 
         assertThat(cleanString(testString)).isEqualTo("HelloWorld");
+    }
+
+    @Test
+    void shouldReturnAllClassVariablesInStringFormat() throws IllegalArgumentException, IllegalAccessException {
+
+        var testClass = ImageEntity.builder().title("test-title").url("test-url").index(0)
+                .description("test-description").build();
+
+        var stringResponse = StringHelper.classToString(testClass);
+
+        assertThat(stringResponse).contains("title=test-title");
+        assertThat(stringResponse).contains("url=test-url");
+        assertThat(stringResponse).contains("index=0");
+        assertThat(stringResponse).contains("description=test-description");
+
+    }
+
+    @Test
+    void shouldReturnAllSuperClassVariablesInStringFormat() throws IllegalArgumentException, IllegalAccessException {
+
+        var id = UUID.randomUUID();
+        var createdAt = OffsetDateTime.now();
+        var updatedAt = OffsetDateTime.now().plusDays(1L);
+
+        var testClass = ImageEntity.builder().id(id).createdAt(createdAt).updatedAt(updatedAt).build();
+
+        var stringResponse = StringHelper.classToString(testClass);
+
+        assertThat(stringResponse).contains("id=" + id);
+        assertThat(stringResponse).contains("createdAt=" + createdAt);
+        assertThat(stringResponse).contains("updatedAt=" + updatedAt);
+
+
     }
 }
