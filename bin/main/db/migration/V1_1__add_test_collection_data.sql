4b7e71be0602bdcd5fa59d021a77b57f585380b7
Robert <robertdoneux@gmail.com>
2021-06-08 19:50:11 +0100

    [main] updates database schema for S3

--- bin/main/db/migration/V1_1__add_test_collection_data.sql
+++ bin/main/db/migration/V1_1__add_test_collection_data.sql
@@ -3,9 +3,7 @@
 ---------------------
 INSERT INTO collection (id, title, index, tag_colour, tag_title, title_image)
 VALUES 
-('e3228e77-b31b-40e6-9bfe-0e4359fb1455', 'Animals', 0, '#5d5d5d', 'Nature', 0),
-('f8e6f439-e655-496c-8a50-b868ac933ecf', 'Urban', 1, '#5d5d5d', 'Urban', 0),
-('58c59c51-5b83-4342-af7e-fa33579033f8', 'Wild Life', 2, '#5d5d5d', 'Nature', 0),
-('c3441915-3654-46cc-8fb9-bcb50fbbe731', 'Random Images', 3, '#7baab5', 'Random', 0),
-('9f002d09-20f7-4094-bbae-2ed1d0e19c2e', 'People', 4, '#5d5d5d', 'Nature', 0),
-('c91a680d-54dc-444e-9e38-079a66c21f2f', 'Places', 5, '#5d5d5d', 'Urban', 0);
+('e3228e77-b31b-40e6-9bfe-0e4359fb1455', 'Urban', 0, '#5d5d5d', 'Nature', 0),
+('f8e6f439-e655-496c-8a50-b868ac933ecf', 'Nature', 0, '#5d5d5d', 'Urban', 0),
+('58c59c51-5b83-4342-af7e-fa33579033f8', 'Animals', 0, '#5d5d5d', 'Nature', 0),
+('9f002d09-20f7-4094-bbae-2ed1d0e19c2e', 'All Images', 0, '#5d5d5d', 'Nature', 0);
